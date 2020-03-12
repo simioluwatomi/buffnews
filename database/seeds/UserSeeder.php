959ccab50879de6420636efbf0763996253d886c
simioluwatomi <deifilius1@gmail.com>
2020-03-12 19:11:54 +0100

    Formatting fixes

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class ModelsNewsSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run()
     {
+        factory(User::class, 10)->create();
     }
 }
