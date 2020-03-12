c121483a2e1f3e74b09f0439c0f145fbd37905b2
simioluwatomi <deifilius1@gmail.com>
2020-03-12 15:06:50 +0100

    Move web authentication routes into a separate route file

--- routes/auth.php
+++ routes/auth.php
@@ -1,21 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Auth Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where web authentication routes of the application are defined. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-//Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes(['register' => false]);
